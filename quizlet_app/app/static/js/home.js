946a466d60f24ba0caade9479d82ce7e1eb97895
Spelljinxer <reiden556@gmail.com>
2022-09-14 19:05:56 +0800

    [:camera:] image changes + added dropdown, transition later

--- quizlet_app/app/static/js/home.js
+++ quizlet_app/app/static/js/home.js
@@ -1,8 +1,13 @@
 
 
-var images = ['bg-1.png', 'bg-2.png', 'bg-3.png', 'bg-4.png', 'bg-5.png', 'bg-6.png', 'bg-7.png'];
+var images = ['bg-1.png', 'bg-2.png', 'bg-3.png', 'bg-4.png', 'bg-5.png', 'bg-6.png'];
     $('body').css({
         'background-image': 'url(' + '../static/images/' + images[Math.floor(Math.random() * images.length)] + ')',
     });
 
 
+    function myFunction(x) {
+        x.classList.toggle("change");
+        
+        document.getElementById('myDropdown').style.display = document.getElementById('myDropdown').style.display =='block' ?   'none' : 'block';
+    }
