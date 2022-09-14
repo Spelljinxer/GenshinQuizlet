4c196b738b56f0b4fcd90df23e36a75d201ad558
Spelljinxer <reiden556@gmail.com>
2022-09-14 17:39:29 +0800

    menu icon added, next make it dropdown

--- quizlet_app/app/static/js/home.js
+++ quizlet_app/app/static/js/home.js
@@ -1,4 +1,8 @@
 
 
-var images = ['bg-1.jpg', 'bg-2.jpg', 'bg-3.png', 'bg-4.jpg'];
+var images = ['bg-1.png', 'bg-2.png', 'bg-3.png', 'bg-4.png', 'bg-5.png', 'bg-6.png', 'bg-7.png'];
+    $('body').css({
+        'background-image': 'url(' + '../static/images/' + images[Math.floor(Math.random() * images.length)] + ')',
+    });
+
 
