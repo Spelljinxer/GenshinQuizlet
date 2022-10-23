72a36e3ebe5645e51b0e9e0129655339a25634bb
Sckaeth <sckaeth@gmail.com>
2022-10-23 17:35:48 +0800

    Basic question rendering

--- quizlet_app/app/models.py
+++ quizlet_app/app/models.py
@@ -12,11 +12,17 @@ class Image(db.Model):
     name = db.Column(db.String(64))
 
 class Question(db.Model):
+    # The first element in the answers list will always be the correct answer.
     id = db.Column(db.Integer, primary_key=True)
     topic = db.Column(db.Integer, db.ForeignKey('topic.id'))
     image = db.Column(db.Integer, db.ForeignKey('image.id'))
     text = db.Column(db.Text, index=True)
     answers = db.Column(db.Text, default=json.dumps(["","","",""]))
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Integer, db.ForeignKey('question.id'))
+    answer = db.Column(db.Integer)
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- quizlet_app/app/routes.py
+++ quizlet_app/app/routes.py
@@ -1,5 +1,9 @@
 from flask import render_template
-from app import app
+from app import app, db
+from app.models import Topic, Image, Question, Answer, Game
+from datetime import datetime
+import json
+import random
 
 @app.route('/')
 def index():
@@ -7,4 +11,28 @@ def index():
 
 @app.route('/quiz')
 def quiz():
-    return render_template('quiz.html')
+    # Number of questions (hard-coded at 4 for testing)
+    num_q = 4
+
+    # Gets a list of every topic.
+    topics = Topic.query.all()
+
+    # Randomly samples n questions from questions in a certain topic.
+    questions = []
+    for topic in topics:
+        question = random.sample(Question.query.filter_by(topic=topic.id).all(), min(int(num_q/len(topics)), 1))
+        questions.extend(question)
+
+    # Shuffles the list of questions.
+    random.shuffle(questions)
+    # Gets a list of images.
+    images = []
+    for question in questions:
+        image_name = Image.query.filter_by(id=question.image).first().name
+        images.append(image_name)
+
+    # Create a list of topics in ascending order of ids.
+    for index, topic in enumerate(topics):
+        topics[index] = topic.name
+
+    return render_template('quiz.html', questions=questions, images=images, topics=topics)
