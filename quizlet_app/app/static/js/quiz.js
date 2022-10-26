1d67c5512e14655c9d1894ebcc4d84b90e6eb89a
Spelljinxer <reiden556@gmail.com>
2022-10-26 15:26:53 +0800

    世界で一番おひめさま

--- quizlet_app/app/static/js/quiz.js
+++ quizlet_app/app/static/js/quiz.js
@@ -4,3 +4,13 @@ function hi(shit) {
         alert(listan[i]);
     }
 }
+
+var images = ['bg-1.png', 'bg-2.png', 'bg-3.png', 'bg-4.png', 'bg-5.png', 'bg-6.png'];
+    $('body').css({
+        'background-image': 'url(' + '../static/images/' + images[Math.floor(Math.random() * images.length)] + ')',
+        'background-size': 'cover',
+        'background-repeat': 'no-repeat',
+        'webkit-background-size': 'cover',
+        'moz-background-size': 'cover',
+        'o-background-size': 'cover',
+    });
