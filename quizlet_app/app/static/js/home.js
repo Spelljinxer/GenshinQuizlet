68e133e1a447676a176633d67265b8352b4fce94
Spelljinxer <reiden556@gmail.com>
2022-10-30 23:04:05 +0800

    redoc

--- quizlet_app/app/static/js/home.js
+++ quizlet_app/app/static/js/home.js
@@ -2,7 +2,6 @@
 
 /**
  * Creates a randomised background image
- * TODO: Make it fit to screen (!!)
  * TODO: Add more images (!)
  */
  var images = ['bg-1.png', 'bg-2.png', 'bg-3.png', 'bg-4.png', 'bg-5.png', 'bg-6.png'];
